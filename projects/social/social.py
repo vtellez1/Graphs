2402e1a5202ccd7bfadfdf9328ab2d4155e4f278
Vanessa <vanessatellez45@gmail.com>
2020-05-13 18:19:43 -0400

    Social part 1&2 complete

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,3 +1,18 @@
+import random
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+
 class User:
     def __init__(self, name):
         self.name = name
@@ -45,8 +60,28 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(0, num_users):
+            self.add_user(f"User {i}")
 
         # Create friendships
+        #Generate all possible friendship possibilities:
+        possible_friendships = []
+        #Avoid duplicates by ensuring the first number is smaller than the second
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        #Shuffle the possible friendships
+        random.shuffle(possible_friendships)
+
+        #Create friendships for the first X pairs of the list
+        # X is determind by the formula: num_users * avg_friendships // 2
+        # Need to divide by 2 since each add_friendship() creates 2 friendships
+
+        for i in range(num_users * avg_friendships // 2):
+            friendship = possible_friendships[i]
+            self.add_friendship(friendship[0], friendship[1])
+
 
     def get_all_social_paths(self, user_id):
         """
@@ -55,10 +90,64 @@ class SocialGraph:
         Returns a dictionary containing every user in that user's
         extended network with the shortest friendship path between them.
 
+        Breadth-First Traversal -- QUEUE -- FIFO
+            Returns a list containing the shortest path from start to finish
+
         The key is the friend's ID and the value is the path.
         """
         visited = {}  # Note that this is a dictionary, not a set
-        # !!!! IMPLEMENT ME
+
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        q = Queue()
+        q.enqueue(user_id)
+
+        #Key is friend's ID : Value is path
+        #Path so far is the user id
+        visited[user_id] = [user_id]
+
+        # While the queue is not empty...
+        while q.size() > 0:
+            #Dequeue the first PATH
+            user = q.dequeue()
+
+            #Get current user's friends
+            #friendships is also a dict, use user_id as key
+            friends = self.friendships[user]
+
+            #Loop through friends
+            for friend in friends:
+                #If not in visited...
+                if friend not in visited:
+                    #Add to dict with friend's id as Key, and path as Value
+                    #path so far is what is in Visited's value thus far and current friend that we're on
+                    visited[friend] = visited[user] + [friend]
+                    #Add friend to queue
+                    q.enqueue(friend)
+
+    """
+***** USED BFS as reference *****
+     def bfs(self, starting_vertex, destination_vertex):
+
+        # Create an empty queue and enqueue A PATH TO the starting vertex ID
+        q = Queue()
+        q.enqueue([starting_vertex])
+
+        # Create a Set to store visited vertices
+        visited = set()
+
+        # While the queue is not empty...
+        while q.size() > 0:
+            #Dequeue the first PATH
+            path = q.dequeue()
+
+                # Then add A PATH TO its neighbors to the back of the queue
+                for next_vert in self.get_neighbors(last_ver):
+                    # _COPY_ THE PATH                    
+                    prev_path = list(path)
+                    # APPEND THE NEIGHOR TO THE BACK                    
+                    prev_path.append(next_vert)
+                    q.enqueue(prev_path) 
+    """      
         return visited
 
 
@@ -68,3 +157,14 @@ if __name__ == '__main__':
     print(sg.friendships)
     connections = sg.get_all_social_paths(1)
     print(connections)
+
+"""
+Questions
+
+1.) To create 100 users with an average of 10 friends each, 
+how many times would you need to call add_friendship()? Why?
+
+2.) If you create 1000 users with an average of 5 random friends each, 
+what percentage of other users will be in a particular user's extended social network? 
+What is the average degree of separation between a user and those in his/her extended network?
+"""
